cc2b3995612c01e870c39264cc288c88d5986fa9
Doncho Minkov <Doncho.Minkov@telerik.com>
2013-10-08 09:02:52 -0500

    Changes made by Doncho Minkov (Doncho.Minkov@telerik.com).

--- Places-App/scripts/app/categories.js
+++ Places-App/scripts/app/categories.js
@@ -1,14 +1,33 @@
 var app = app || {};
 
-(function(a){
-    function init(){
-        httpRequest.getJSON(app.servicesBaseUrl+"categories")
-            .then(function(categories){
-                  $("#categories-holder").html(JSON.stringify(categories));              
-            });
-    }  
-    a.categories={
-      init:init  
-      init:init  
+(function(a) {
+    function init(e) {
+        var dataSource = new kendo.data.DataSource({
+            data:[
+                {Name:"sample", Id:1},
+                {Name:"sample 2", Id:2}, 
+                {Name: "sample 3", Id :3}
+            ],
+            change:onCategoryChanged
+        });
+        var viewModel = kendo.observable({
+            categories:dataSource           
+        });
+        
+        kendo.bind(e.view.element, viewModel);
+    }
+    
+    function onCategoryChanged() {
+        var categoryId = $("#categories-list").val();
+        /*httpRequest.getJSON("http://localhost:62354/api/" + "categories/"+categoryId)
+        .then(function(categoryInfo){
+        $("#category-info").html(JSON.stringify(categoryInfo)); 
+        });
+        */
+        navigator.notification.alert(categoryId);
+    }
+    
+    a.categories = {
+        init:init          
     };
 }(app));
