afb8ea880c5ed166b9c85ab7a55da5308f47db4f
Mariela Kasovska <mariela.kasovska@gmail.com>
2013-10-13 05:46:39 -0500

    Changes made by Mariela Kasovska (mariela.kasovska@gmail.com).

---
+++ Places-App/scripts/app/checkConnection.js
@@ -0,0 +1,24 @@
+    function check() {
+        var networkState = navigator.network.connection.type;
+
+        var states = {};
+        states[Connection.UNKNOWN] = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI] = 'WiFi connection';
+        states[Connection.CELL_2G] = 'Cell 2G connection';
+        states[Connection.CELL_3G] = 'Cell 3G connection';
+        states[Connection.CELL_4G] = 'Cell 4G connection';
+        states[Connection.NONE] = 'No network connection';
+        
+        if(states[networkState]=='Unknown connection' || states[networkState]=='No network connection'){
+            return false;
+        }
+       
+            return true;
+        
+    }
+    
+    return {
+       check: check
+    }
+}());
